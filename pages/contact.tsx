0f597bd5a2bb7640a0812460a6ccbfe50a1602a9
kenta640 <kenta0803d@gmail.com>
2022-03-12 15:34:11 +1300

    contact updated

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -16,7 +16,17 @@ function Contact() {
             https://www.linkedin.com/in/kenta-okubo-315a8620a/
           </div>
         </Link>
+        <Link href="https://www.facebook.com/people/Kenta-Okubo/100006493073503/">
+          <div className="font-mono text-3xl">
+            My Facebook
+          </div>
 
+        </Link>
+        <Link href="https://www.facebook.com/people/Kenta-Okubo/100006493073503/">
+          <div>
+            https://www.facebook.com/people/Kenta-Okubo/100006493073503/
+          </div>
+        </Link>
       </div>);
 }
  export default Contact
