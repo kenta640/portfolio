62efdaaa884ca6965ed14e252c5a785121bd683d
kenta640 <kenta0803d@gmail.com>
2023-03-24 14:03:32 +1300

    urls added

--- pages/myProjects.js
+++ pages/myProjects.js
@@ -4,6 +4,8 @@ function MyProjects({pizzas}) {
     return (
       <div>
       <Navbar/>
+      <div>Full stack applications are not working due to maintenance cost</div>
+      <div class="py-8 ...">
       <Link href="https://svelte-front.vercel.app/">
       <div className="font-mono text-4xl text-blue-300 hover:text-blue-700">
         Simple Social media
@@ -17,7 +19,8 @@ function MyProjects({pizzas}) {
       <Link href="https://svelte-front.vercel.app/">
           https://svelte-front.vercel.app/
       </Link>
-      
+      </div>
+      <div class="py-8 ...">
       <br/>
       <div className="text-red-300">Caution!! If you click the link below .exe file will be downloaded (not malicious!!)</div>
       <Link href="https://storage.googleapis.com/password-generator-deluxe/releases/Password_Generator_Deluxe%20Setup%201.0.0.exe">
@@ -29,7 +32,30 @@ function MyProjects({pizzas}) {
       <Link href="https://storage.googleapis.com/password-generator-deluxe/releases/Password_Generator_Deluxe%20Setup%201.0.0.exe">
         https://storage.googleapis.com/password-generator-deluxe/releases/Password_Generator_Deluxe%20Setup%201.0.0.exe
       </Link>
+      </div>
+      <div class="py-8 ...">
+      <Link href="https://stylegarage.co.nz/">
+        <div className="font-mono text-4xl text-blue-300 hover:text-blue-700">
+          Style Garage (Shopify website)
+        </div>
+      </Link>
 
+      <Link href="https://stylegarage.co.nz/">
+      https://stylegarage.co.nz
+      </Link>
+      </div>
+      <div class="py-8 ...">
+      <Link href="https://legendary-granita-9100d6.netlify.app/">
+        <div className="font-mono text-4xl text-blue-300 hover:text-blue-700">
+          Novel Website (Oath2)
+        </div>
+      </Link>
+
+      <Link href="https://legendary-granita-9100d6.netlify.app/">
+      https://legendary-granita-9100d6.netlify.app/
+      </Link>
+      </div>
+      <div class="py-8 ...">
       <Link href="https://github.com/kenta640">
         <div className="font-mono text-4xl text-blue-300 hover:text-blue-700">
           My Github repositories
@@ -39,7 +65,7 @@ function MyProjects({pizzas}) {
       <Link href="https://github.com/kenta640">
         https://github.com/kenta640
       </Link>
-
+      </div>
       </div>);
 }
  export default MyProjects;
